1a36eafe487d39fb07c07c03f21bdc905d2adba1
김성찬 <sungchan.kim@datastreams.co.kr>
2021-09-16 14:41:49 +0900

    hello world

--- src/app.js
+++ src/app.js
@@ -17,8 +17,11 @@ app.post('/', async(req, res) => {
 })
 
 app.post('/slack/events', async(req, res) => {
-    res.send(req.body.challenge)
-    await send(`event: ${req.body.event}, type: ${req.body.type}, text: ${req.body.event.text}`)
+    if(req.body.type === 'event_callback' && req.body.event.type === "message") {
+        if(req.body.event.text === 'Hello')
+        await send(`World!`)
+    }
+    res.sendStatus(200)
 })
 
 app.listen(PORT, () => {
